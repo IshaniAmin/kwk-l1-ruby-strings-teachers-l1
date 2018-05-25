8c9d4d1c65ff73ce6a6abf90f26dae76d366415b
Ishani <ishaniamin@gmail.com>
2018-05-25 01:29:28 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -13,3 +13,12 @@ time = gets.chomp
 
 puts "Host name"
 host_name = gets.chomp.capitilize
+
+
+puts "Dear Hermione,
+
+You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
+
+Sincerely,
+
+Harry Potter"
