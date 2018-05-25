1c44ddd8926131eab8bb72ec8e52176945c096c1
Ishani <ishaniamin@gmail.com>
2018-05-25 01:39:59 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,17 +1,17 @@
 puts "Hi, you've been invited to a party! What is your name?"
-guest_name = gets.chomp.capitalize
+guest_name = gets.chomp.split(/ |\_|\-/).map(&:capitalize).join(" ")
 
 puts "Enter party name"
 party_name = gets.chomp.split(/ |\_|\-/).map(&:capitalize).join(" ")
 
 puts "Enter date"
-date = gets.chomp.capitalize
+date = gets.chomp.split(/ |\_|\-/).map(&:capitalize).join(" ")
 
 puts "Enter time"
 time = gets.chomp
 
 puts "Host name"
-host_name = gets.chomp.capitalize
+host_name = gets.chomp.split(/ |\_|\-/).map(&:capitalize).join(" ")
 
 
 puts "Dear #{guest_name},
